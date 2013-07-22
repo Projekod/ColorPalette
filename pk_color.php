853902cc8dbdda61827e0bb888da7ac45187f376
Ali GÜNDOĞDU <ali@vuska.net>
2013-07-22 12:15:24 +0300

    Color Palette

---
+++ pk_color.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Projekod Interactive
+ * http://projekod.com
+ */
+
+
+class pk_color {
+
+    protected $granularity = 5;
+
+    protected $picture = "";
+
+    protected $colors = array();
+
+    protected $colorsCount = 10;
+
+
+    public function setGranularity($val){
+        $this->granularity = $val;
+        return $this;
+    }
+
+    public function setPicture($val){
+        $this->picture = $val;
+        return $this;
+    }
+
+    public function setColorsCount($val){
+        $this->colorsCount = $val;
+        return $this;
+    }
+
+
+    public function test(){
+        echo $this->picture."<br/>";
+        echo $this->granularity."<br/>";
+        echo $this->colorsCount."<br/>";
+    }
+
+    public function getIt($picture=""){
+
+        if(!empty($picture)){$this->picture = $picture;}
+
+        if(!file_exists($this->picture)){
+            throw new Exception("Resim dosyası bulunamadı.");
+        }
+
+        $_granularity = max(1, abs((int)$this->granularity));
+
+        $_sizes = @getimagesize($this->picture);
+
+        if($_sizes === false){
+            throw new Exception("Resim boyuları alınamadı.");
+        }
+
+        $_img = @imagecreatefromstring(file_get_contents($this->picture));
+
+        if(!$_img){
+            throw new Exception("Resim dosyası hatalı.");
+        }
+
+
+        for($First = 0; $First < $_sizes[0]; $First += $this->granularity)
+        {
+            for($Second = 0; $Second < $_sizes[1]; $Second += $this->granularity)
+            {
+                $thisColor = imagecolorat($_img, $First, $Second);
+                $rgb = imagecolorsforindex($_img, $thisColor);
+                $red = round(round(($rgb['red'] / 0x33)) * 0x33);
+                $green = round(round(($rgb['green'] / 0x33)) * 0x33);
+                $blue = round(round(($rgb['blue'] / 0x33)) * 0x33);
+                $thisRGB = sprintf('%02X%02X%02X', $red, $green, $blue);
+                if(array_key_exists($thisRGB, $this->colors))
+                {
+                    $this->colors[$thisRGB]++;
+                }
+                else
+                {
+                    $this->colors[$thisRGB] = 1;
+                }
+            }
+        }
+
+
+        unset($_granularity);
+        unset($_sizes);
+        unset($_img);
+
+        arsort($this->colors);
+        return array_slice(array_keys($this->colors), 0, $this->colorsCount);
+
+    }
+}
---
+++ test.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Color Palette Test</title>
+
+    <style>
+        body {
+            margin: 0px;padding:0px;
+        }
+        .box {
+            width: 25%;
+            height: 200px;
+            float: left;
+            margin:0px;
+            padding:0px;
+            text-align: center;
+
+        }
+        .image {
+            margin:10px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+<div class='image'>
+    <img src='test.jpg' alt='color palette test image' />
+</div>
+
+<?php
+require "pk_color.php";
+
+$pkc = new pk_color();
+$palette = $pkc->setGranularity(10)
+    ->setPicture("test.jpg")
+    ->setColorsCount(30)
+    ->getIt();
+
+
+?>
+
+<?php
+    foreach($palette as $color):
+?>
+    <div class='box' style="background:#<?=$color?> ">#<?=$color?></div>
+<?php
+    endforeach;
+?>
+</body>
+</html>
